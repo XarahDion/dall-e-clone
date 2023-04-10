27428542d025bc1b61760e9a89eb2aba3e0cdbdb
Xarah Dion <xarahdion@gmail.com>
2023-04-10 18:54:18 -0400

    showcase and download

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -21,8 +21,31 @@ const RenderCards: React.FC<RenderCardsProps> = ({ data, title }) => {
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
-    const [allPosts, setaAllPosts] = useState(null);
+    const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/post", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                if (response.ok) {
+                    const result = await response.json();
+                    setAllPosts(result.data.reverse());
+                }
+            } catch (error) {
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchPosts();
+    }, []);
 
     return (
         <section className="max-w-7xl mx-auto">
@@ -50,9 +73,9 @@ const Home = () => {
                         )}
                         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                             {searchText ? (
-                                <RenderCards data={[]} title="No Search results found" />
+                                <RenderCards data={allPosts} title="No Search results found" />
                             ) : (
-                                <RenderCards data={[]} title="No posts found" />
+                                <RenderCards data={allPosts} title="No posts found" />
                             )}
                         </div>
                     </>
